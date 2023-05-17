efc411368362141cb30bbda600cf5403415719d8
Shaun <shaun.laidlow@gmail.com>
2023-05-16 19:36:44 -0700

    Create Operate Function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum
+let secondNum
+let operator
+
 function add(a,b){
     return a + b;
 }
@@ -12,4 +16,23 @@ function multiply(a,b){
 
 function divide(a,b){
     return a / b;
+}
+
+function operate(firstNum,operator, secondNum){
+    switch(operator){
+        case "+":
+            return add(firstNum,secondNum);
+            break;
+        case "-":
+            return subtract(firstNum,secondNum);
+            break;
+        case "*":
+            return multiply(firstNum,secondNum);
+            break;
+        case "/":
+            return divide(firstNum,secondNum);
+            break;
+        default:
+            return "Uh Oh"
+    }
 }
