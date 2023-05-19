b24a4a868ed3c78471cc4f5499223aaf3fb7c8a0
Shaun <shaun.laidlow@gmail.com>
2023-05-19 07:39:23 -0700

    ALLOW USER TO ENTER MORE THEN ONE OPERATOR
    Lets user string multiple equations together

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let secondNum = ""
 let operator = ""
 
 function add(a,b){
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a,b){
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply(a,b){
@@ -21,6 +21,7 @@ function divide(a,b){
 function operate(firstNum,operator, secondNum){
     switch(operator){
         case "+":
+            console.log("++++")
             return add(firstNum,secondNum);
             break;
         case "-":
@@ -42,12 +43,13 @@ current = document.querySelector(".current")
 previous = document.querySelector(".previous")
 let pastResults = ""
 let currentDisplay = ""
+let enterSecondDigit = false
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         
-        
+        //USER CLICKS CLEAR
         if(button.id == "clear"){
-
+            console.log(`clear`)
             firstNum = ""
             operator = ""
             secondNum = ""
@@ -55,33 +57,45 @@ buttons.forEach(button => {
             previous.textContent= ""
             currentDisplay=""
             pastResults = ""
+            enterSecondDigit = false
         }
+        //USER CLICKS FIRST NUMBER
         else if(!button.classList.contains("operator") && operator == ""){
-            console.log("first")
             previous.textContent = pastResults
             firstNum += button.textContent
             currentDisplay += button.textContent
             current.textContent = currentDisplay
+            console.log(`first num = ${firstNum}`)
         }
-        else if(button.classList.contains("operator") && firstNum != ""){
-            console.log("oppo")
+
+        //FIRST OPERATOR SIGN
+        else if(button.classList.contains("operator") && firstNum != "" && enterSecondDigit == false){
             operator = button.textContent
             currentDisplay += button.textContent
             current.textContent = currentDisplay
+            console.log(`operator = ${operator}`)
         }
+
+        //SELECT OPERATOR WITHOUT SELECT FIRST NUMBER
         else if(button.classList.contains("operator") && firstNum == ""){
-            console.log("second oppo")
             if(pastResults != ""){
                 previous.textContent = pastResults
                 firstNum += pastResults
                 operator = button.textContent
+                enterSecondDigit = false
+                console.log(`first num = ${firstNum} and operator = ${operator}`)
                 currentDisplay += firstNum
                 currentDisplay += button.textContent
                 current.textContent = currentDisplay
             }
+            else{
+                currentDisplay = "ERROR"
+                current.textContent = currentDisplay
+            }
         }
+
+        //USER PRESSES EQUALS
         else if(button.classList.contains("equals")){
-            console.log("eqs")
             let result = operate(firstNum,operator,secondNum);
             currentDisplay = result
             current.textContent = currentDisplay
@@ -91,14 +105,42 @@ buttons.forEach(button => {
             secondNum = ""
             pastResults = result
             currentDisplay=""
+            console.log(`it equals = ${result}`)
         }
-        else if(!button.classList.contains("operator") && operator != ""){
-            console.log("sec")
-            secondNum += button.textContent
+
+         
+        //USER ENTERS SECOND OPERATOR
+        else if(button.classList.contains("operator") && secondNum != ""){
+            console.log("    ")
+            console.log(firstNum)
+            console.log(operator)
+            console.log(secondNum)
+            console.log("    ")
+            firstNum = operate(firstNum,operator,secondNum);
+            
+            operator = button.textContent
+            console.log(`first num = ${firstNum} operator = ${operator}`)
+            secondNum = ""
             currentDisplay += button.textContent
             current.textContent = currentDisplay
-            
+            enterSecondDigit = false
         }
+
+        //USER ENTERS SECOND NUMBER
+        else if(!button.classList.contains("operator") && operator != ""){
+            secondNum += button.textContent
+            console.log(`second num = ${secondNum}`)
+            currentDisplay += button.textContent
+            current.textContent = currentDisplay
+            enterSecondDigit = true;
+        }
+
+        
+        
+
+        
+
+        
         
         else{
             alert("Error Will Robinson")
