241e073e634f6b9d0502c5bbf794d45a220be912
Shaun <shaun.laidlow@gmail.com>
2023-05-18 21:05:09 -0700

    LINK FUNCTIONALITY OF FUNCTIONS TO BUTTONS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstNum
-let secondNum
-let operator
+let firstNum = ""
+let secondNum = ""
+let operator = ""
 
 function add(a,b){
     return a + b;
@@ -26,13 +26,87 @@ function operate(firstNum,operator, secondNum){
         case "-":
             return subtract(firstNum,secondNum);
             break;
-        case "*":
+        case "x":
             return multiply(firstNum,secondNum);
             break;
-        case "/":
+        case "÷":
             return divide(firstNum,secondNum);
             break;
         default:
             return "Uh Oh"
     }
 }
+
+buttons = document.querySelectorAll("button");
+current = document.querySelector(".current")
+previous = document.querySelector(".previous")
+let pastResults = ""
+let currentDisplay = ""
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        
+        if(button.id == "clear"){
+
+            firstNum = ""
+            operator = ""
+            secondNum = ""
+            current.textContent= ""
+            previous.textContent= ""
+            currentDisplay=""
+            pastResults = ""
+        }
+        else if(!button.classList.contains("operator") && operator == ""){
+            console.log("first")
+            previous.textContent = pastResults
+            firstNum += button.textContent
+            currentDisplay += button.textContent
+            current.textContent = currentDisplay
+        }
+        else if(button.classList.contains("operator") && firstNum != ""){
+            console.log("oppo")
+            operator = button.textContent
+            currentDisplay += button.textContent
+            current.textContent = currentDisplay
+        }
+        else if(button.classList.contains("operator") && firstNum == ""){
+            console.log("second oppo")
+            if(pastResults != ""){
+                previous.textContent = pastResults
+                firstNum += pastResults
+                operator = button.textContent
+                currentDisplay += firstNum
+                currentDisplay += button.textContent
+                current.textContent = currentDisplay
+            }
+        }
+        else if(button.classList.contains("equals")){
+            console.log("eqs")
+            let result = operate(firstNum,operator,secondNum);
+            currentDisplay = result
+            current.textContent = currentDisplay
+
+            firstNum = ""
+            operator = ""
+            secondNum = ""
+            pastResults = result
+            currentDisplay=""
+        }
+        else if(!button.classList.contains("operator") && operator != ""){
+            console.log("sec")
+            secondNum += button.textContent
+            currentDisplay += button.textContent
+            current.textContent = currentDisplay
+            
+        }
+        
+        else{
+            alert("Error Will Robinson")
+        }
+        
+
+        
+        
+    })
+    
+});
